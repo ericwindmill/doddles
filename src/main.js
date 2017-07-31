87ad862da5f6a70028d44bb2b1c9ddb6b99b1f04
Eric Windmill <ericwindmill@gmail.com>
2017-07-31 16:09:02 -0700

    Add firebase

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueFire from 'vuefire'
+import firebase from './firebase'
+import firebaseui from 'firebaseui'
 import router from './router'
 import App from './App'
 
 Vue.use(Vuex)
+Vue.use(VueFire)
+
 Vue.config.productionTip = true
 
 /* eslint-disable no-new */
